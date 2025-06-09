e6d525ecc8a95cbd194a1d0ef9045a8692e47978
XULIM <not3lbobillibo@gmail.com>
2025-06-09 02:58:16 -0400

    ppm_grayscale working

--- src/main.c
+++ src/main.c
@@ -7,58 +7,33 @@
 #include <immintrin.h>
 
 
-U0 test(U0)
-{
-    Arena *arena = arena_alloc(8 * 1024);
-    
-    float *arr1 = (float*)arena_push(arena, alignof(float),
-            3 * sizeof(float), NOZERO);
-    float *arr2 = (float*)arena_push(arena, alignof(float),
-            3 * sizeof(float), NOZERO);
-
-    arr1[0] = 4.0;
-    arr1[1] = 3.0;
-    arr1[2] = 2.0;
-
-    arr2[0] = 7.0;
-    arr2[1] = 8.0;
-    arr2[2] = 9.0;
-
-    __m128 vec1, vec2, sum, mul;
-    
-    vec1 = _mm_load_ps(arr1);
-    vec2 = _mm_load_ps(arr2);
-
-    sum = _mm_add_ps(vec1, vec2);
-    mul = _mm_mul_ps(vec1, vec2);
-
-    _mm_storeu_ps(arr1, sum);
-    _mm_storeu_ps(arr2, mul);
-
-    printf("Sum\n");
-    for (int i = 0; i < 3; i++)
-        printf("%.2f ", arr1[i]);
-    printf("\n");
-
-    printf("Product\n");
-    for (int i = 0; i < 3; i++)
-        printf("%.2f ", arr2[i]);
-    printf("\n");
-
-    arena_release(arena);
-}
-
-
 I32 main(U0)
 {
-    PPMImage *ppmimage;
+    PPMImage *ppmimage, *grayimage;
     Arena *arena;
 
     // reserve 10 MB of space
     arena = arena_alloc(10 * 1024 * 1024);
 
     ppmimage = ppm_read(IMG_FOLDER"new.ppm", arena);
+    grayimage = ppm_grayscale(ppmimage, arena);
 
+    printf("%2s\n%lu %lu\n%lu\n",
+            grayimage->header,
+            grayimage->width,
+            grayimage->height,
+            grayimage->max_color);
+
+    for (U64 i = 0; i < grayimage->width * grayimage->height; i++)
+    {
+        printf("%hhu %hhu %hhu\n",
+                grayimage->pixels[i].r,
+                grayimage->pixels[i].g,
+                grayimage->pixels[i].b);
+    }
+
+
+    /*
     printf("width: %lu\nheight: %lu\nmax_color: %lu\n",
             ppmimage->width,
             ppmimage->height,
@@ -71,6 +46,9 @@ I32 main(U0)
                 ppmimage->pixels[i].g,
                 ppmimage->pixels[i].b);
     }
+    */
+
+    ppm_grayscale(ppmimage, arena);
 
     arena_release(arena);
     return 0;

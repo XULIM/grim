96107124be6065b6f67a4c06ae45971f3923a4a9
XULIM <not3lbobillibo@gmail.com>
2025-06-09 02:57:53 -0400

    ppm_grayscale now works, considering to make ppmproc modular

--- src/ppmproc.c
+++ src/ppmproc.c
@@ -4,19 +4,22 @@
 #include "ppmproc.h"
 
 #include <string.h>
+#include <immintrin.h>
 
 #define NOB_IMPLEMENTATION
 
 #define IS_VALID_PIXEL(p, maxval)\
     ( ( (p.r) > (maxval) || (p.g) > (maxval) || (p.b) > (maxval) ) ? 0 : 1 )
 
+// Allow the functions to manage memory on arena,
+// or keep it modular?
+
 PPMImage* ppm_read(const C8 *restrict path, Arena *restrict arena)
 {
-    U8 r, g, b;
-    U16 width, height, maxval;
     U64 original_offset, i;
     FILE *file;
     PPMImage *ppmimage;
+    U16 width, height, maxval;
 
     // Open PPM file
     file = fopen(path, "r");
@@ -93,4 +96,70 @@ PPMImage* ppm_read(const C8 *restrict path, Arena *restrict arena)
 
     fclose(file);
     return ppmimage;
+}
+
+PPMImage* ppm_grayscale(PPMImage *image, Arena *arena)
+{
+    __m128 COEFF_R, COEFF_G, COEFF_B, r, g, b;
+    __m128 grays;
+    F32 temp[4];
+    U64 size, i, j;
+    Pixel *gray_pixels;
+    PPMImage *gray_image;
+    U8 temp_color;
+
+    // Denoting Y as the gray value,
+    // it is computed as Y = 0.2989R + 0.5870G + 0.1140B
+
+    COEFF_R = _mm_set1_ps(0.299f);
+    COEFF_G = _mm_set1_ps(0.587f);
+    COEFF_B = _mm_set1_ps(0.114f);
+
+    size = (image->width) * (image->height);
+
+    gray_pixels = new(arena, Pixel, size, NOZERO);
+    gray_image = new(arena, PPMImage, size, NOZERO);
+
+    // Load 4 RGB values at the same time
+    for (i = 0; i < size; i += 4)
+    {
+        r = _mm_set_ps((F32)image->pixels[i].r,
+                (F32)image->pixels[i+1].r,
+                (F32)image->pixels[i+2].r,
+                (F32)image->pixels[i+3].r);
+        g = _mm_set_ps((F32)image->pixels[i].g,
+                (F32)image->pixels[i+1].g,
+                (F32)image->pixels[i+2].g,
+                (F32)image->pixels[i+3].g);
+        b = _mm_set_ps((F32)image->pixels[i].b,
+                (F32)image->pixels[i+1].b,
+                (F32)image->pixels[i+2].b,
+                (F32)image->pixels[i+3].b);
+
+        // Here: grays = (0.299r + 0.587g) + 0.114b
+        grays = _mm_add_ps(_mm_add_ps(
+                    _mm_mul_ps(COEFF_R, r),
+                    _mm_mul_ps(COEFF_G, g)),
+                    _mm_mul_ps(COEFF_B, b));
+
+        _mm_store_ps(temp, grays);
+
+        // Copy F32 gray values from temp to Pixel array
+        for (j = 0; j < 4; j++)
+        {
+            temp_color = (U8)grays[j];
+            gray_pixels[i+j].r = temp_color;
+            gray_pixels[i+j].g = temp_color;
+            gray_pixels[i+j].b = temp_color;
+        }
+    }
+
+    // Still use P3 since these are RGB values
+    strcpy(gray_image->header, "P3");
+    gray_image->width = image->width;
+    gray_image->height = image->height;
+    gray_image->max_color = image->max_color;
+    gray_image->pixels = gray_pixels;
+
+    return gray_image;
 }
--- src/ppmproc.h
+++ src/ppmproc.h
@@ -6,19 +6,26 @@ typedef enum
     READALL = 1,
 } PPM_Proc_Flag;
 
-typedef struct
+typedef struct // 3 byte wide
 {
     U8 r, g, b;
 } Pixel;
 
-typedef struct
+typedef struct // 40 byte wide on 64-bit
 {
-    C8 header[2];
     U64 width, height;
     U64 max_color;
     Pixel *pixels;
+    C8 header[2];
 } PPMImage;
 
+/* Reads P3 PPM into PPMImage struct, which is allocated on the arena.
+ */
 PPMImage* ppm_read(const C8 *restrict path, Arena *restrict arena);
+
+
+U0 ppm_write(const C8 *restrict path, PPMImage *image);
+
+PPMImage* ppm_grayscale(PPMImage *image, Arena *arena);
 
 #endif
